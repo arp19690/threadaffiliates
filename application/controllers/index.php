7201756a754ecef717ae3602da13773d8a241af8
Arpit Maheshwari <arp19690@gmail.com>
2017-03-03 15:54:06 +0530

    change

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -18,14 +18,14 @@ class Index extends CI_Controller
         $custom_model = new Custom_model();
 
         $home_decor_products = $custom_model->get_all_products_for_category(10);
-        $best_sellers = $model->fetchSelectedData("*", TABLE_PRODUCTS, array("product_status" => 1), "rand()", "rand()", "0,12");
+        $most_viewed = $model->fetchSelectedData("*", TABLE_PRODUCTS, array("product_status" => 1), "rand()", "rand()", "0,12");
         $featured_products = $model->fetchSelectedData("*", TABLE_PRODUCTS, array("product_status" => 1, "product_featured" => 1), "rand()", "rand()", "0,12");
 
         $page_title = "Home - " . SITE_NAME;
         $data["page_title"] = $page_title;
         $data['meta_title'] = $data["page_title"];
         $data['home_decor_products'] = $home_decor_products;
-        $data['best_sellers'] = $best_sellers;
+        $data['most_viewed'] = $most_viewed;
         $data['featured_products'] = $featured_products;
         $this->template->write_view("content", "pages/index/index", $data);
         $this->template->render();
