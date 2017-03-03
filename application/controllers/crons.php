d437fe863344e5d00194716678f01152a6951166
Arpit Maheshwari <arp19690@gmail.com>
2017-03-03 12:11:26 +0530

    update sitemap automatically

--- application/controllers/crons.php
+++ application/controllers/crons.php
@@ -11,7 +11,7 @@ class Crons extends CI_Controller
         parent::__construct();
     }
 
-    public function fetch($type=NULL)
+    public function fetch($type = "flipkart")
     {
         try
         {
@@ -31,6 +31,32 @@ class Crons extends CI_Controller
         {
             echo 'Caught exception: ', $e->getMessage(), "\n";
         }
+    }
+
+    public function update_sitemap()
+    {
+        $model = new Common_model();
+
+        $xml = '<?xml version = "1.0" encoding = "UTF-8"?>' . "\n";
+        $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">' . "\n";
+        $xml .= '<url><loc>' . base_url() . '</loc><lastmod>' . date('Y-m-d') . 'T' . date('H:i:s') . '+00:00</lastmod><changefreq>daily</changefreq><priority>1.00</priority></url>' . "\n";
+
+        // all the active products
+        $product_records = $model->fetchSelectedData('product_url_key', TABLE_PRODUCTS, array('product_status' => '1'));
+        foreach ($product_records as $pValue)
+        {
+            $product_url = getTripUrl($pValue['product_url_key']);
+            $xml .= '<url><loc>' . $product_url . '</loc><lastmod>' . date('Y-m-d') . 'T' . date('H:i:s') . '+00:00</lastmod><changefreq>daily</changefreq><priority>0.85</priority></url>' . "\n";
+        }
+
+        $xml .= '</urlset>';
+//            prd($xml);
+
+        $file = fopen((APPPATH . '/../sitemap.xml'), 'w');
+        fwrite($file, $xml);
+        fclose($file);
+        echo "Sitemap generated successfully";
+        die;
     }
 
 }
