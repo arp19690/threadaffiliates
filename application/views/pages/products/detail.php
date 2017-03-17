73b7612d73531065017de4b8a840f367f48835f4
Arpit Maheshwari <arp19690@gmail.com>
2017-03-17 12:42:52 +0530

    changing ad placements

--- application/views/pages/products/detail.php
+++ application/views/pages/products/detail.php
@@ -135,6 +135,8 @@ echo isset($breadcrumb) ? $breadcrumb : "";
                     </div>    
                 </div>
 
+                <div class="clearfix g-ad"><?php echo get_google_ad(); ?></div>
+
                 <script type="text/javascript">
                     jQuery(function ($) {
                         "use strict";
@@ -199,44 +201,44 @@ echo isset($breadcrumb) ? $breadcrumb : "";
 
 <script src="<?php echo JS_PATH; ?>/jquery.zoom.min.js" type="text/javascript"></script>
 <script type="text/javascript">
-                    jQuery(function ($) {
-                        var num_slides = 3;
-                        if ($(window).width() <= 1199)
-                            num_slides = 3;
-                        if ($(window).width() <= 991)
-                            num_slides = 3;
-                        if ($(window).width() <= 768)
-                            num_slides = 3;
-                        if ($(window).width() <= 480)
-                            num_slides = 2;
-                        if ($(window).width() <= 400)
-                            num_slides = 1;
-                        $('.thumb-widget .slides').bxSlider({
-                            mode: 'vertical',
-                            minSlides: num_slides,
-                            maxSlides: num_slides,
-                            pager: false,
-                            controls: true,
-                            slideMargin: 10
-                        });
+                jQuery(function ($) {
+                    var num_slides = 3;
+                    if ($(window).width() <= 1199)
+                        num_slides = 3;
+                    if ($(window).width() <= 991)
+                        num_slides = 3;
+                    if ($(window).width() <= 768)
+                        num_slides = 3;
+                    if ($(window).width() <= 480)
+                        num_slides = 2;
+                    if ($(window).width() <= 400)
+                        num_slides = 1;
+                    $('.thumb-widget .slides').bxSlider({
+                        mode: 'vertical',
+                        minSlides: num_slides,
+                        maxSlides: num_slides,
+                        pager: false,
+                        controls: true,
+                        slideMargin: 10
+                    });
 
-                        $('.img-thumb > a').click(function (event) {
-                            $('#ProductPhotoImg').attr('src', $(this).attr('data-image'));
+                    $('.img-thumb > a').click(function (event) {
+                        $('#ProductPhotoImg').attr('src', $(this).attr('data-image'));
 
-                            $('.img-thumb > a').removeClass('shown');
-                            $(this).addClass('shown');
+                        $('.img-thumb > a').removeClass('shown');
+                        $(this).addClass('shown');
 
-                            $('#ProductPhotoImg').attr('data-zoom', $(this).attr('data-image'));
+                        $('#ProductPhotoImg').attr('data-zoom', $(this).attr('data-image'));
 //                                $('#ProductPhoto').zoom({
 //                                    url: $(this).find('#ProductPhotoImg').attr('data-zoom')
 //                                });
-                            event.preventDefault();
-                        });
+                        event.preventDefault();
+                    });
 
 //                            $('#ProductPhoto').zoom({
 //                                url: $(this).find('#ProductPhotoImg').attr('data-zoom')
 //                            });
-                    });
+                });
 </script> 
 
 <?php
