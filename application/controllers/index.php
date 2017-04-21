d6e58e627f77778c0099885ae1cc0bf46d355850
Arpit Maheshwari <arp19690@gmail.com>
2017-04-21 12:36:50 +0530

    adding women fashion on homepage

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -17,6 +17,7 @@ class Index extends CI_Controller
 //        $model = new Common_model();
         $custom_model = new Custom_model();
 
+        $women_apparels_products = $custom_model->get_all_products_for_category(8);
         $home_decor_products = $custom_model->get_all_products_for_category(10);
         $smartphone_products = $custom_model->get_all_products_for_category(12);
         $camera_products = $custom_model->get_all_products_for_category(61);
@@ -25,10 +26,13 @@ class Index extends CI_Controller
         $page_title = "Home - " . SITE_NAME;
         $data["page_title"] = $page_title;
         $data['meta_title'] = $data["page_title"];
+        
+        $data['women_apparels_products'] = $women_apparels_products;
         $data['home_decor_products'] = $home_decor_products;
         $data['smartphone_products'] = $smartphone_products;
         $data['camera_products'] = $camera_products;
 //        $data['featured_products'] = $featured_products;
+        
         $this->template->write_view("content", "pages/index/index", $data);
         $this->template->render();
     }
